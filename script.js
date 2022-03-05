17388aa3f935c9f061691ae2baa210efceda0a9c
Cameron Chardukian <camchardukian@gmail.com>
2022-03-05 17:50:11 -0600

    feature/legend

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         .attr("height", height);
 
     const EDUCATION_SCALE = {
+        ZERO: 0,
         TWELVE: 12,
         TWENTY_ONE: 21,
         THIRTY: 30,
@@ -24,14 +25,24 @@ document.addEventListener("DOMContentLoaded", async () => {
         SIXTY_SIX: 66,
     };
 
+    const COLORS_SCALE = {
+        LIGHTEST: "#e5f5e0",
+        LIGHTER: "#c7e9c0",
+        LIGHT: "#a1d99b",
+        BASELINE: "#74c476",
+        DARK: "#41ab5d",
+        DARKER: "#238b45",
+        DARKEST: "#006d2c",
+    }
+
     const handleApplyFillColor = (percentHoldingDegree) => {
-        if (percentHoldingDegree <= EDUCATION_SCALE.TWELVE) return "#e5f5e0";
-        if (percentHoldingDegree <= EDUCATION_SCALE.TWENTY_ONE) return "#c7e9c0";
-        if (percentHoldingDegree <= EDUCATION_SCALE.THIRTY) return "#a1d99b";
-        if (percentHoldingDegree <= EDUCATION_SCALE.THIRTY_NINE) return "#74c476";
-        if (percentHoldingDegree <= EDUCATION_SCALE.FORTY_EIGHT) return "#41ab5d";
-        if (percentHoldingDegree <= EDUCATION_SCALE.FIFTY_SEVEN) return "#238b45";
-        if (percentHoldingDegree <= EDUCATION_SCALE.SIXTY_SIX) return "#006d2c";
+        if (percentHoldingDegree < EDUCATION_SCALE.TWELVE) return COLORS_SCALE.LIGHTEST;
+        if (percentHoldingDegree < EDUCATION_SCALE.TWENTY_ONE) return COLORS_SCALE.LIGHTER;
+        if (percentHoldingDegree < EDUCATION_SCALE.THIRTY) return COLORS_SCALE.LIGHT;
+        if (percentHoldingDegree < EDUCATION_SCALE.THIRTY_NINE) return COLORS_SCALE.BASELINE;
+        if (percentHoldingDegree < EDUCATION_SCALE.FORTY_EIGHT) return COLORS_SCALE.DARK;
+        if (percentHoldingDegree < EDUCATION_SCALE.FIFTY_SEVEN) return COLORS_SCALE.DARKER;
+        else return COLORS_SCALE.DARKEST;
     };
 
     svg.append("g")
@@ -53,4 +64,32 @@ document.addEventListener("DOMContentLoaded", async () => {
         .attr("d", path)
         .attr("class", "state")
 
+
+
+    const legendScale = d3.scaleLinear()
+        .domain([0, 100])
+        .range([width - (1 / 3 * width), width - padding]);
+
+    const thresholdPercentagesArray = Object.values(EDUCATION_SCALE)
+    const legendWidth = width - (2 / 3 * width) - padding;
+
+    const legend = svg.append("g")
+        .attr("id", "legend")
+
+    legend.selectAll("rect")
+        .data(thresholdPercentagesArray)
+        .enter()
+        .append("rect")
+        .attr("x", d => legendScale(d))
+        .attr("y", (padding / 2.5) - 20)
+        .attr("width", legendWidth / 2.925)
+        .attr("height", 20)
+        .style("fill", d => handleApplyFillColor(d));
+
+    const legendAxis = d3.axisBottom(legendScale)
+        .ticks(thresholdPercentagesArray.length)
+
+    svg.append("g")
+        .attr("transform", "translate(0," + padding / 2.5 + ")")
+        .call(legendAxis);
 })
